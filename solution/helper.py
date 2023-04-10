c14a670120cab9a0e3e0e7a611afaa0c05def2b5
Kaseke <kasymkhankhubievnis@gmail.com>
2023-04-10 12:02:05 +0700

    started project

---
+++ solution/helper.py
@@ -0,0 +1,13 @@
+"""
+    В этом модуле реализованы функции, которые парсят данные, 
+    обрабатывают и выдают в нужном виде.
+"""
+
+from typing import NamedTuple
+
+class Runner(NamedTuple):
+    id: str
+
+
+def get_runners_data(file: str):
+    pass
